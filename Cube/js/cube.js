97440805990607c86ab15b848bef52e5d86c8169
Keerny <zxq-1234@mail.ru>
2021-12-24 23:38:38 +0300

    Добавил анимацию

--- Cube/js/cube.js
+++ Cube/js/cube.js
@@ -8,6 +8,7 @@ class Cube {
         this.ctx.x = 50;
         this.ctx.y = 50;
         this.agle = 10;
+        this.m = this.ctx.y;
     }
     // update(cnt) {
     //     this.ctx.x = 50;
@@ -23,27 +24,33 @@ class Cube {
     update(cnt) {
         if (cnt === 0) {
             this.ctx.y = 0;
+            this.ctx.x = 0;
         } if (cnt % 5 === 0) {
-            this.ctx.y += 10;
-            this.ctx.x = 530;
+            this.ctx.y += 100;
+            // this.ctx.x = 530;
         } else {
             this.ctx.x += 80;
         }
     }
 
     drow() {
-        this.agle += 0.1
+        this.agle += 0.05
         this.ctx.save();
         this.ctx.strokeStyle = Utils.generateColor();
         // this.ctx.strokeStyle = "#ffffffff";
-        this.ctx.translate(this.ctx.x + this.wight / 2,this.ctx.y + this.height / 2);
+        this.ctx.translate(this.ctx.x + this.wight / 2,this.m + this.height / 2);
         this.ctx.rotate(this.agle);
-        this.ctx.translate(-(this.ctx.x + this.height / 2),-(this.ctx.y + this.height / 2))
-        this.ctx.strokeRect(this.ctx.x, this.ctx.y, this.wight, this.height);
-
-
+        this.ctx.translate(-(this.ctx.x + this.height / 2),-(this.m + this.height / 2))
+        this.wight += Math.random() - 0.5;
+        this.height += Math.random() - 0.5;
+        this.m += Math.random()*10;
+        this.ctx.strokeRect(this.ctx.x, this.m, this.wight, this.height);
+        if (this.m >= 1000) {
+            this.m = 0
+        }
 
         this.ctx.restore();
+
     }
 }
 export default Cube;
